1d6da4c2d1ef69743ad7bd35ad2f0be4e267a48f
aath1ra <130980704+aath1ra@users.noreply.github.com>
2024-07-13 11:59:42 +0530

    Add files via upload

---
+++ filemaker.py
@@ -0,0 +1,42 @@
+
+while True:
+    print("Would you like to write or read into a file ?")
+    key=input("Press 'w' to write, 'r' to read, 'x' to escape: ")
+    
+    if key == 'x':
+        break
+    if key == 'r':
+        f1=input("Please write the name of the file you would like to read: ")
+        with open(f1, "r") as f:
+            print(f.read())
+            
+            
+    if key == 'w':
+        f2=input("Please write the name of the file you would like to create: ")
+        wr=input("Write the message you want to print on the file: ")
+        with open(f2, "w") as file:
+            file.write(wr)
+            print("Your message has been written!")
+        res=input("Would you like to see the printed message?: ")
+        
+        if res == 'yes':
+            with open(f2, "r") as file:
+                content=file.read()
+                print(content)
+                
+        else:
+            break
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
